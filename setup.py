8c7e04daf08442c4751efb59fdee21e4a7e8c926
Mathieu de Bony <lavergne@lapp.in2p3.fr>
2022-07-01 15:51:13 +0200

    Increase version number

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
 setup(
     name='acceptance_modelisation',
     packages=find_packages(),
-    version='0.1.0-dev',
+    version='0.1.1-dev',
     description='Calculate 2D acceptance model for creating maps with gammapy (IACT analysis)',
     long_description=long_description,
     long_description_content_type='text/markdown',
