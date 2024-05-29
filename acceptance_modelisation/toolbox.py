369a62f8b5a4b814ba747993eb54cbe09e5c887d
Marie-Sophie Carrasco <carrasco@cppm.in2p3.fr>
2024-05-29 15:06:49 +0200

    added wobble identification function for cos zenith binning method

--- acceptance_modelisation/toolbox.py
+++ acceptance_modelisation/toolbox.py
@@ -1,9 +1,11 @@
 import numpy as np
 import astropy.units as u
-from astropy.coordinates import AltAz, SkyCoord
+from astropy.coordinates import AltAz, SkyCoord, angular_separation
 from astropy.coordinates.earth import OMEGA_EARTH, EarthLocation
 from astropy.time import Time
 import matplotlib.pyplot as plt
+from gammapy.data import Observations
+from copy import deepcopy
 
 def compute_rotation_speed_fov(time_evaluation: Time,
                                pointing_sky: SkyCoord,
@@ -32,6 +34,47 @@ def compute_rotation_speed_fov(time_evaluation: Time,
         pointing_altaz.alt)
     return omega
 
+def get_unique_wobble_pointings(observations: Observations, max_angular_separation=0.4):
+    """
+    Compute the angular separation between pointings and return a list
+    of detected wobbles with their associated similar pointings
+
+    Parameters
+    ----------
+    observations : gammapy.data.observations.Observations
+        The list of observations
+    max_angular_separation : float
+        The maximum angular separation between identified wobbles, in degrees
+
+    Returns
+    -------
+    unique_wobble_list : list
+        A list of the wobbles detected and their associated similar pointings (angular separation < 0.4°)
+    """
+    all_ra_observations = np.array([obs.get_pointing_icrs(obs.tmid).ra.to_value(u.deg) for obs in observations])
+    all_dec_observations = np.array([obs.get_pointing_icrs(obs.tmid).dec.to_value(u.deg) for obs in observations])
+    ra_observations = deepcopy(all_ra_observations)
+    dec_observations = deepcopy(all_dec_observations)
+    wobbles = np.empty(shape=len(all_ra_observations), dtype=np.object_)
+    wobbles_dict = {}
+    i=0
+    mask_allremaining = np.ones(shape=len(all_ra_observations),dtype=bool)
+    while len(ra_observations)>0:
+        i=i+1
+        keywobble='W'+str(i)
+        mask = (angular_separation(ra_observations[0]*u.deg, dec_observations[0]*u.deg,
+                                   ra_observations*u.deg, dec_observations*u.deg) < max_angular_separation*u.deg)
+        mask_2 = (angular_separation(np.mean(ra_observations[mask])*u.deg, np.mean(dec_observations[mask])*u.deg,
+                                     all_ra_observations*u.deg, all_dec_observations*u.deg) < max_angular_separation*u.deg)
+        wobbles_dict[keywobble] = [np.mean(all_ra_observations[mask_2 & mask_allremaining]), np.mean(all_dec_observations[mask_2 & mask_allremaining])]
+        wobbles[mask_2 & mask_allremaining] = keywobble
+        mask_allremaining = mask_allremaining * ~mask_2
+        ra_observations = all_ra_observations[mask_allremaining]
+        dec_observations = all_dec_observations[mask_allremaining]
+
+    print(f"{len(wobbles_dict)} wobbles were found: \n", wobbles_dict)
+    return wobbles
+
 def plot_coszd_binning(cos_zenith_observations,cos_zenith_bin,bin_center,livetime_observations,min_cut_per_cos_zenith_bin,cos_zenith_binning_method,zd_lim=(55,75)):
     fig,ax=plt.subplots(figsize=(5,5))
     ax.hist(cos_zenith_observations, bins=cos_zenith_bin,weights=livetime_observations,alpha=0.6)
@@ -59,7 +102,7 @@ def plot_coszd_binning(cos_zenith_observations,cos_zenith_bin,bin_center,livetim
     ylim=ax.get_ylim()    
     ax.vlines(cos_zenith_bin,ylim[0],ylim[1],ls=':',color='grey',label='bin edges',alpha=0.5)
     ax.legend(loc='best')
-    
+
     plt.suptitle("Livetime per cos(zd) bin\n")
     plt.tight_layout()
     plt.show()
