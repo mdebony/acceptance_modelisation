f2612a3abe7ca5d141843be37400f1aefa6a1ec6
Gabriel Emery <gabriel-emery@hotmail.fr>
2024-07-10 16:32:37 +0200

    Initialise logging config. Fix string in some log messages.

--- acceptance_modelisation/__init__.py
+++ acceptance_modelisation/__init__.py
@@ -2,3 +2,6 @@ from .base_radial_acceptance_map_creator import BaseRadialAcceptanceMapCreator
 from .base_acceptance_map_creator import BaseAcceptanceMapCreator
 from .grid3d_acceptance_map_creator import Grid3DAcceptanceMapCreator
 from .radial_acceptance_map_creator import RadialAcceptanceMapCreator
+
+import logging
+logging.basicConfig()
