7da651e94e0947faa5c0d2eb49a72e52cd1475a7
Mathieu de Bony <mathieu.debony@cea.fr>
2024-07-16 14:56:55 +0200

    Add  an object to store a collection of background model that are energy dependant

--- acceptance_modelisation/__init__.py
+++ acceptance_modelisation/__init__.py
@@ -2,3 +2,4 @@ from .base_radial_acceptance_map_creator import BaseRadialAcceptanceMapCreator
 from .base_acceptance_map_creator import BaseAcceptanceMapCreator
 from .grid3d_acceptance_map_creator import Grid3DAcceptanceMapCreator
 from .radial_acceptance_map_creator import RadialAcceptanceMapCreator
+from .bkg_collection import BackgroundCollectionZenith
