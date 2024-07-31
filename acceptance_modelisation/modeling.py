f19983afd98b28bc9701e83f0c72f8365f39090f
Mathieu de Bony <mathieu.debony@cea.fr>
2024-07-31 16:27:17 +0200

    Add copyright text at top of the files

--- acceptance_modelisation/modeling.py
+++ acceptance_modelisation/modeling.py
@@ -1,3 +1,15 @@
+# -*- coding: utf-8 -*-
+# -------------------------------------------------------------------
+# Filename: modeling.py
+# Purpose: Collection of function for fitting analytic model of background
+# Author: Gabriel Emery
+# Copyright (C) 2023-2024
+#
+# This program is free software: you can redistribute it and/or modify it under the terms of the GNU Lesser General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+# This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more details.
+# ---------------------------------------------------------------------
+
+
 """
 Define the background model functions for fits and associated seeds and bounds.
 """
